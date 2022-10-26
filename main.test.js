9ca4c8fe5dc1a5bf3d27f8674c8136969761e349
sardarz <muhkween@gmail.com>
2022-10-26 20:40:46 +0300

    add wrong test

--- main.test.js
+++ main.test.js
@@ -2,5 +2,5 @@ const {sumTwoNumbers} = require("./main")
 
 it("test", () => {
   expect(sumTwoNumbers(2, 5)).toBe(7)
-  expect(sumTwoNumbers(5, 5)).toBe(10)
+  expect(sumTwoNumbers(5, 5)).toBe(109)
 })
