f7830e3e9fdac05636dde6c5adb41e81da508ea4
sardarz <muhkween@gmail.com>
2022-10-28 01:34:25 +0300

    ...

--- showGithubInfo.js
+++ showGithubInfo.js
@@ -1,3 +1,20 @@
 const github = require("@actions/github")
+const exec = require("@actions/exec")
 
-console.log(github)
+let myOutput = ''
+let myError = ''
+const options = {}
+options.listeners = {
+  stdout: (data) => {
+    myOutput += data.toString();
+  },
+  stderr: (data) => {
+    myError += data.toString();
+  }
+}
+// options.cwd = './lib';
+ exec.exec('ls', ['-1'], options).then(() => {
+   console.log("OUTPUT", myOutput)
+
+ })
+
